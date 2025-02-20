549308a4b3390a498e87c51507c6011408076114
cheesypotat0e <cheesypotatoe75@gmail.com>
2025-02-20 06:42:01 -0800

    fix key type

--- oauth/main.py
+++ oauth/main.py
@@ -151,7 +151,7 @@ def root(request):
 
         # Store tokens in Firestore
         db = firestore.Client()
-        doc_ref = db.collection("users").document(user_id)
+        doc_ref = db.collection("users").document(str(user_id))
         doc_ref.set(
             {
                 "user_id": user_id,
