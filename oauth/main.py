e421f46f389ff23bb9e7c55ed3fee8cc82cc1cf8
cheesypotat0e <cheesypotatoe75@gmail.com>
2025-02-20 01:33:51 -0800

    Fix get user

--- oauth/main.py
+++ oauth/main.py
@@ -133,7 +133,7 @@ def root(request):
 
         data = response.json()
 
-        user_id = data.get("data").get("user_id")
+        user_id = data.get("data")[0].get("user_id")
 
         access_token = res.get("access_token")
         refresh_token = res.get("refresh_token")
